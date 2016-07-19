40981f27f313791fba308500ab8094b1c02c8431
javenkn <javenkn7@yahoo.com>
2016-07-19 00:27:34 -1000

    used SELECT DISTINCT instead of GROUP BY

--- denormal_cars.sql
+++ denormal_cars.sql
@@ -14,20 +14,19 @@ ALTER DATABASE denormal_cars OWNER TO denormal_user;
 
 -- In denormal.sql Create a query to get a list of all make_title values in the car_models table, without any duplicate rows.
 -- (should have 71 results)
-SELECT make_title FROM car_models
+SELECT DISTINCT make_title FROM car_models
   GROUP BY make_title;
 
 -- In denormal.sql Create a query to list all model_title values where the make_code is 'VOLKS', without any duplicate rows
 -- (should have 27 results)
-SELECT model_title FROM car_models
+SELECT DISTINCT model_title FROM car_models
   WHERE make_code = 'VOLKS'
   GROUP BY model_title;
 
 -- In denormal.sql Create a query to list all make_code, model_code, model_title, and year from car_models where the make_code
 -- is 'LAM' (should have 136 rows)
-SELECT make_code, model_code, model_title, year FROM car_models
-  WHERE make_code = 'LAM'
-  GROUP BY make_code, model_code, model_title, year;
+SELECT DISTINCT make_code, model_code, model_title, year FROM car_models
+  WHERE make_code = 'LAM';
 
 -- In denormal.sql Create a query to list all fields from all car_models in years between 2010 and 2015 (should have 7884 rows)
 SELECT * FROM car_models
